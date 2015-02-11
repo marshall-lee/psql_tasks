a8bd5d8e4a34c9b3e7423b95f4155f9ce642f515
Vladimir Kochnev <hashtable@yandex.ru>
2015-02-11 13:15:16 +0300

    refactor, add padrino support

--- lib/psql_tasks.rb
+++ lib/psql_tasks.rb
@@ -1,4 +1,19 @@
 module PsqlTasks
+  class << self
+    def padrino?
+      defined? PadrinoTasks
+    end
+
+    def activerecord?
+      ar = defined? ActiveRecord
+      ar || (padrino? && PadrinoTasks.load?(:activerecord, ar))
+    end
+
+    def sequel?
+      sq = defined? Sequel
+      sq || (padrino? && PadrinoTasks.load?(:sequel, sq))
+    end
+  end
 end
 
 require 'psql_tasks/config'
--- lib/psql_tasks/rake.rb
+++ lib/psql_tasks/rake.rb
@@ -3,5 +3,5 @@ require 'psql_tasks'
 load 'psql_tasks/rake/terminal.rake'
 load 'psql_tasks/rake/gen_pgpass.rake'
 load 'psql_tasks/rake/config.rake'
-load 'psql_tasks/rake/activerecord_config.rake' if defined?(ActiveRecord)
-load 'psql_tasks/rake/sequel_config.rake' if defined?(Sequel)
+load 'psql_tasks/rake/activerecord_config.rake' if PsqlTasks.activerecord?
+load 'psql_tasks/rake/sequel_config.rake' if PsqlTasks.sequel?
